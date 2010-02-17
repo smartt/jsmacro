c2ea0c6f8772d2b28c2957196a4062a2049512ef
Erik Smartt <github@eriksmartt.com>
2010-02-17 11:55:51 -0600

    added a test for a non-closing macro

---
+++ testfiles/nonclosing-macro-in.js
@@ -0,0 +1,8 @@
+//@define DEBUG 1
+
+var foo = function() {
+  //@if DEBUG
+  alert('starting...');
+
+  var bar = "Hello World";
+};
---
+++ testfiles/nonclosing-macro-out.js
@@ -0,0 +1,8 @@
+
+
+var foo = function() {
+  //@if DEBUG
+  alert('starting...');
+
+  var bar = "Hello World";
+};
